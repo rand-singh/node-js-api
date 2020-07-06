da4079bb9747318c8b339667244cdce44f2f987c
Randeep Singh <rand-singh@users.noreply.github.com>
2020-07-06 21:56:53 +0100

    Update app.js
    Using node js core modules - http

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,4 +1,12 @@
 const { sum } = require('./helpers');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('hello from node http server');
+});
+
+server.listen(3000);
+
 
 const total = sum(2,333);
 console.log("Total: ", total);
