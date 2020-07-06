03617069aebb9f352a94f053b219bc5bb33d6575
Randeep Singh <rand-singh@users.noreply.github.com>
2020-07-06 22:05:05 +0100

    configured nodemon

--- node-basic/app.js
+++ node-basic/app.js
@@ -2,7 +2,7 @@ const { sum } = require('./helpers');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('hello from node http server update');
+    res.end('hello from node http server updatesss');
 });
 
 server.listen(3000);
