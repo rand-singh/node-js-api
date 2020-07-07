4169c53311a51cde371bf7af217e3b0bb57665fa
Randeep Singh <rand-singh@users.noreply.github.com>
2020-07-07 22:22:38 +0100

    Separating routes

--- node-api/app.js
+++ node-api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+
+// import routes
+const { getPosts } = require('./routes/post');
+
+app.get('/', getPosts);
+
 const port = 3000;
-
-app.listen(port, () => console.log(`A Node JS API is listening on port: ${port}`));
-
-app.get('/', (req, res) => {
-    res.send('hello world from node js');
-})
+app.listen(port, () => {
+    console.log(`A Node JS API is listening on port: ${port}`)
+});
 
