dc59b1536ee17e5719cb4f498acb89e7d6f23093
Randeep Singh <rand-singh@users.noreply.github.com>
2020-07-07 21:34:48 +0100

    Read file - event loop

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const { response } = require('express');
-const app = express();
+// const express = require('express');
+// const { response } = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello from express')
-});
+// app.get('/', (req, res) => {
+//     res.send('hello from express')
+// });
 
-app.listen(3000);
+// app.listen(3000);
+
+const fs = require('fs'),
+    fileName = 'target.txt';
+
+fs.watch(fileName, () => console.log('File changed!'));
