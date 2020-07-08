734eb32484e0db9b5a070071eacdf510aa765a0e
Randeep Singh <rand-singh@users.noreply.github.com>
2020-07-08 16:00:20 +0100

    Setup Mongo DB

--- node-api/app.js
+++ node-api/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// db config
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err}`)
+});
 
 // import routes
 const postRoutes = require('./routes/post');
@@ -16,7 +27,7 @@ app.use(myOwnMiddleware);
 
 app.use('/', postRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node JS API is listening on port: ${port}`)
 });
